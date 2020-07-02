33cb34bd5a78788a4b83fb893a6fc1f080f0777f
machi <machi@jcfc.cn>
2020-07-02 15:42:37 +0800

    bug fix

--- src/main/java/com/machi/daily/gittest/GitTest.java
+++ src/main/java/com/machi/daily/gittest/GitTest.java
@@ -3,5 +3,6 @@ package com.machi.daily.gittest;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
+        System.out.println("bug fix");
     }
 }
